0768da49108712193f530c945d8b585d74d49b91
Jon Gossett <jogosset@adobe.com>
2026-08-17 16:48:52 -0500

    AI: sync files

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -4,7 +4,7 @@ import {
 import { h } from '@dropins/tools/preact.js';
 import { events } from '@dropins/tools/event-bus.js';
 import { tryRenderAemAssetsImage } from '@dropins/tools/lib/aem/assets.js';
-import { getConfigValue, getHeaders } from '@dropins/tools/lib/aem/configs.js';
+import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import * as pdpApi from '@dropins/storefront-pdp/api.js';
 import { render as pdpRendered } from '@dropins/storefront-pdp/render.js';
 import { render as wishlistRender } from '@dropins/storefront-wishlist/render.js';
